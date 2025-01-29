08c34b8dca62c34a588281f964451643b47ce5dd
Pourya Salimi <pouryasalimi1@gmail.com>
2025-01-29 15:49:02 +0330

    delete function

--- Array.cpp
+++ Array.cpp
@@ -5,20 +5,27 @@ const int SIZE = 4;
 int arr[SIZE] = {0, 0, 0, 0};
 
 void inarr() {
-    int s = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < s; i++) {
+    for (int i = 0; i < SIZE; i++) {
         cout << arr[i] << endl;
     }
 }
 
 void insertarr(int arr[], int key, int place) {
     for (int i = SIZE - 1; i > place; i--) {
-        arr[i] = arr[i-1];
+        arr[i] = arr[i - 1];
     }
     arr[place] = key;
 }
 
+void delarr(int arr[], int rmplace) {
+    for (int i = rmplace; i < SIZE - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr[SIZE - 1] = 0; // Reset the last element to zero
+}
+
 int main() {
+    cout << "Initial array: " << endl;
     inarr();
 
     int key = 1; // what we want to insert
@@ -26,8 +33,15 @@ int main() {
 
     insertarr(arr, key, place);
     
-    cout << "Array after insertion:" << endl;
+    cout << "Array after insertion: " << endl;
     inarr();
 
+    int rmplace = 2; // where should be deleted
+
+    delarr(arr, rmplace);
+
+    cout << "Array after deletion: " << endl;
+    inarr();
+    
     return 0;
 }
